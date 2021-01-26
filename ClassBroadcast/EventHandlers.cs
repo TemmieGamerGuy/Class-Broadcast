a77cc9c07576100b954653e1807fd26962353bf7
An4r3w <75725607+An4r3w@users.noreply.github.com>
2021-01-26 14:02:47 +0100

    Add files via upload

---
+++ ClassBroadcast/EventHandlers.cs
@@ -0,0 +1,20 @@
+﻿using Exiled.API.Features;
+using Exiled.Events.EventArgs;
+using MEC;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ClassBroadcast
+{
+    public class EventHandlers
+    {
+        public void OnSpawning(SpawningEventArgs ev)
+        {
+            var config = ClassBroadcasts.Instance.Config;
+            if (config.BroadcastsToClass.ContainsKey(ev.RoleType))
+            {
+                ev.Player.Broadcast(duration: config.BroadcastsToClassDurations[ev.RoleType], message: config.BroadcastsToClass[ev.RoleType]);
+            }
+        }
+    }
+}
---
+++ ClassBroadcast/Methods.cs
@@ -0,0 +1,24 @@
+﻿using Exiled.API.Features;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassBroadcast
+{
+    public class Methods
+    {
+        public static void ChaosBroadcast(Player Chaos)
+        {
+            Chaos.ReferenceHub.characterClassManager.NetworkCurClass = RoleType.ChaosInsurgency;
+            Chaos.ClearBroadcasts();
+            Chaos.Broadcast(15, ClassBroadcasts.Instance.Config.BroadcastChaos);
+        }
+
+        internal static void ChaosBroadcast()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
