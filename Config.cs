4d1273add2292bcc7b6283faf3c36c2bcf4aded7
An4r3w <75725607+An4r3w@users.noreply.github.com>
2021-01-26 13:54:43 +0100

    Add files via upload

---
+++ Config.cs
@@ -0,0 +1,17 @@
+﻿using Exiled.API.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassBroadcast
+{
+    public class Config : IConfig
+    {
+        public bool IsEnabled { get; set; } = true;
+        public Dictionary<RoleType, string> BroadcastsToClass { get; set; } = new Dictionary<RoleType, string>() { { RoleType.ClassD, "You are a prisoner slave good luck" } };
+
+        public Dictionary<RoleType, ushort> BroadcastsToClassDurations { get; set; } = new Dictionary<RoleType, ushort>() { { RoleType.ClassD, 3 } };
+    }
+}
