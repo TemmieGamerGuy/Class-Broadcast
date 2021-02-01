d0549bfac1ae0723b56eb742403a645fb4101bed
An4r3w <75725607+An4r3w@users.noreply.github.com>
2021-02-01 09:10:53 +0100

    Updated AssemblyInfo.cs

---
+++ ClassBroadcast/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PrivateBroadcast")]
+[assembly: AssemblyDescription("When a class spawn, in game appear a BC, fully customizable in the configs!")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("An4r3wSans")]
+[assembly: AssemblyProduct("PrivateBroadcast")]
+[assembly: AssemblyCopyright("Copyright © An4r3w Sans 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("f2158d30-1ce2-4b7f-9360-15d3da8ef74f")]
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
